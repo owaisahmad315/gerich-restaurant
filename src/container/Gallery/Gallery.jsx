e78b8cad8837ef7cc8423617763c1e9dee55681a
Owais Ahmad <owaisahmad014@gmail.com>
2023-05-31 12:27:24 +0500

    first commit of the project which is in development

---
+++ src/container/Gallery/Gallery.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Gallery.css';
+
+const Gallery = () => {
+
+  const scrollRef = React.useRef(null);
+ const scroll = (direction) => {
+  const {current} = scrollRef;
+
+  if (direction === 'left') {
+      current.scrollLeft -= 300;
+  } else {
+    current.scrollLeft += 300;
+  }
+ }
+
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{color: "#AAA", marginTop: "2rem"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_contaienr" ref={scrollRef}>
+
+        </div>
+        <div className="app__gallery-images_arrow">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={()=> scroll('left')}/>
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={()=> scroll('right')}/>
+
+        </div>
+
+      </div>
+    
+  </div>
+  )
+}
+ 
+
+export default Gallery;
