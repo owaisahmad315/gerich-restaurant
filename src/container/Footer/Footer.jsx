249e1b0df1cb889900d821ef718f5116a37944cf
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-04 21:54:12 +0500

    started work on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import './Footer.css';
 
 const Footer = () => (
   <div>
-    Footer
+    
   </div>
 );
 
