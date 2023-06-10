3c243630e07ab25d0be4108b8c235062fcce8566
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-10 21:43:25 +0500

    contact us div in footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -10,6 +10,10 @@ const Footer = () => (
     <Newsletter />
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
 
       </div>
       <div className="app__footer-links_logo">
@@ -19,6 +23,9 @@ const Footer = () => (
 
       </div>
 
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
     </div>
   </div>
 );
