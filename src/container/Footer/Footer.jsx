273cf6591b87c8c2d3f7f6d7ee996e726558e041
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-10 21:07:36 +0500

    started work on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,7 +8,18 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
-    
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+
+      </div>
+      <div className="app__footer-links_logo">
+
+      </div>
+      <div className="app__footer-links_work">
+
+      </div>
+
+    </div>
   </div>
 );
 
