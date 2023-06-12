49ef16e4ff55ac5882f2e9c1e312baaa1826ba27
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-12 20:15:12 +0500

    3rd column

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -21,9 +21,18 @@ const Footer = () => (
         <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">"The best way to find yourself is to lose yourself in the service of others.”</p>
         <img src={images.spoon} alt="spoon" className="spoon__img" style={{marginTop: 15}} />
-
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
       </div>
       <div className="app__footer-links_work">
+      <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am-12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00am-11:00am</p>
 
       </div>
 
